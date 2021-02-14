4ccef0cfa69e5eb1c5e02dd626052c679638b8b7
Cornelius Alfredo <33976366+c0rz@users.noreply.github.com>
2021-02-14 20:38:11 +0700

    fix bug + example login

---
+++ example/login.php
@@ -0,0 +1,13 @@
+<?php
+// error_reporting(1);
+
+require '../autoload.php';
+require 'mainconfig.php';
+
+use c0rz\InstagramPHP\Instagram;
+
+$username = USERNAME;
+$password = PASSWORD;
+$instagram = new Instagram();
+$login = json_decode($instagram->login_account($username, $password));
+print_r($login);
